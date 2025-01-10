373d2bde0b53e4a8fd806c945dd821e04829d7ff
Wojciech Kubicki <wojt.kubicki@gmail.com>
2025-01-11 00:33:45 +0100

    test: add tests for powerTwoNumbers method

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,13 +1,15 @@
 package calculator;
 
 
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class CalculatorTest {
 
     private static Calculator calculator;
+    public static final double DELTA = 0;
 
     @BeforeClass
     public static void init() {
@@ -15,8 +17,62 @@ public class CalculatorTest {
     }
 
     @Test
-    public void CalculateRectCircTest() {
-        Assert.assertEquals(8, calculator.CalculateRectCirc(2,2));
+    public void PowerTwoNumbers_PositiveBase_PositiveExponent_Test() {
+        assertEquals(125, calculator.powerTwoNumbers(5,3), DELTA);
     }
 
+    @Test
+    public void PowerTwoNumbers_PositiveBase_NegativeExponent_Test() {
+        assertEquals(0.008, calculator.powerTwoNumbers(5,-3), DELTA);
+    }
+
+    @Test
+    public void PowerTwoNumbers_NegativeBase_PositiveExponent_Test() {
+        assertEquals(-125, calculator.powerTwoNumbers(-5,3), DELTA);
+    }
+
+    @Test
+    public void PowerTwoNumbers_NegativeBase_NegativeExponent_Test() {
+        assertEquals(-0.008, calculator.powerTwoNumbers(-5,-3), DELTA);
+    }
+
+    @Test
+    public void PowerTwoNumbers_PositiveInfinityBase_PositiveExponent_Test() {
+        assertEquals(Double.POSITIVE_INFINITY, calculator.powerTwoNumbers(Double.POSITIVE_INFINITY, 2), DELTA);
+    }
+
+    @Test
+    public void PowerTwoNumbers_PositiveInfinityBase_NegativeExponent_Test() {
+        assertEquals(0, calculator.powerTwoNumbers(Double.POSITIVE_INFINITY, -2), DELTA);
+    }
+
+    @Test
+    public void PowerTwoNumbers_NegativeInfinityBase_PositiveExponent_Test() {
+        assertEquals(Double.POSITIVE_INFINITY, calculator.powerTwoNumbers(Double.NEGATIVE_INFINITY, 2), DELTA);
+    }
+
+    @Test
+    public void PowerTwoNumbers_NegativeInfinityBase_NegativeExponent_Test() {
+        assertEquals(0, calculator.powerTwoNumbers(Double.NEGATIVE_INFINITY, -2), DELTA);
+    }
+
+    @Test
+    public void PowerTwoNumbers_NaNBase() {
+        assertEquals(Double.NaN, calculator.powerTwoNumbers(Double.NaN, 2), DELTA);
+    }
+
+    @Test
+    public void PowerTwoNumbers_NaNExponent() {
+        assertEquals(Double.NaN, calculator.powerTwoNumbers(2, Double.NaN), DELTA);
+    }
+
+    @Test
+    public void PowerTwoNumbers_Overflow() {
+        assertEquals(Double.POSITIVE_INFINITY, calculator.powerTwoNumbers(10, 1000), DELTA);
+    }
+
+    @Test
+    public void PowerTwoNumbers_Underflow() {
+        assertEquals(0, calculator.powerTwoNumbers(10, -1000), DELTA);
+    }
 }
