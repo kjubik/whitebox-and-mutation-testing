96627b0d1cc03b2e9c59bce0af5601ec78fac7bf
Wojciech Kubicki <wojt.kubicki@gmail.com>
2025-01-11 00:40:51 +0100

    test: add test case for sqrt method and change DELTA to 1e-10

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.*;
 public class CalculatorTest {
 
     private static Calculator calculator;
-    public static final double DELTA = 0;
+    public static final double DELTA = 0.00000000001;
 
     @BeforeClass
     public static void init() {
@@ -18,63 +18,66 @@ public class CalculatorTest {
 
     @Test
     public void PowerTwoNumbers_PositiveBase_PositiveExponent_Test() {
-        assertEquals(125, calculator.powerTwoNumbers(5,3), DELTA);
+        assertEquals(125, calculator.powerTwoNumbers(5,3), 0);
     }
 
     @Test
     public void PowerTwoNumbers_PositiveBase_NegativeExponent_Test() {
-        assertEquals(0.008, calculator.powerTwoNumbers(5,-3), DELTA);
+        assertEquals(0.008, calculator.powerTwoNumbers(5,-3), 0);
     }
 
     @Test
     public void PowerTwoNumbers_NegativeBase_PositiveExponent_Test() {
-        assertEquals(-125, calculator.powerTwoNumbers(-5,3), DELTA);
+        assertEquals(-125, calculator.powerTwoNumbers(-5,3), 0);
     }
 
     @Test
     public void PowerTwoNumbers_NegativeBase_NegativeExponent_Test() {
-        assertEquals(-0.008, calculator.powerTwoNumbers(-5,-3), DELTA);
+        assertEquals(-0.008, calculator.powerTwoNumbers(-5,-3), 0);
     }
 
     @Test
     public void PowerTwoNumbers_PositiveInfinityBase_PositiveExponent_Test() {
-        assertEquals(Double.POSITIVE_INFINITY, calculator.powerTwoNumbers(Double.POSITIVE_INFINITY, 2), DELTA);
+        assertEquals(Double.POSITIVE_INFINITY, calculator.powerTwoNumbers(Double.POSITIVE_INFINITY, 2), 0);
     }
 
     @Test
     public void PowerTwoNumbers_PositiveInfinityBase_NegativeExponent_Test() {
-        assertEquals(0, calculator.powerTwoNumbers(Double.POSITIVE_INFINITY, -2), DELTA);
+        assertEquals(0, calculator.powerTwoNumbers(Double.POSITIVE_INFINITY, -2), 0);
     }
 
     @Test
     public void PowerTwoNumbers_NegativeInfinityBase_PositiveExponent_Test() {
-        assertEquals(Double.POSITIVE_INFINITY, calculator.powerTwoNumbers(Double.NEGATIVE_INFINITY, 2), DELTA);
+        assertEquals(Double.POSITIVE_INFINITY, calculator.powerTwoNumbers(Double.NEGATIVE_INFINITY, 2), 0);
     }
 
     @Test
     public void PowerTwoNumbers_NegativeInfinityBase_NegativeExponent_Test() {
-        assertEquals(0, calculator.powerTwoNumbers(Double.NEGATIVE_INFINITY, -2), DELTA);
+        assertEquals(0, calculator.powerTwoNumbers(Double.NEGATIVE_INFINITY, -2), 0);
     }
 
     @Test
     public void PowerTwoNumbers_NaNBase_Test() {
-        assertEquals(Double.NaN, calculator.powerTwoNumbers(Double.NaN, 2), DELTA);
+        assertEquals(Double.NaN, calculator.powerTwoNumbers(Double.NaN, 2), 0);
     }
 
     @Test
     public void PowerTwoNumbers_NaNExponent_Test() {
-        assertEquals(Double.NaN, calculator.powerTwoNumbers(2, Double.NaN), DELTA);
+        assertEquals(Double.NaN, calculator.powerTwoNumbers(2, Double.NaN), 0);
     }
 
     @Test
     public void PowerTwoNumbers_Overflow_Test() {
-        assertEquals(Double.POSITIVE_INFINITY, calculator.powerTwoNumbers(10, 1000), DELTA);
+        assertEquals(Double.POSITIVE_INFINITY, calculator.powerTwoNumbers(10, 1000), 0);
     }
 
     @Test
     public void PowerTwoNumbers_Underflow_Test() {
-        assertEquals(0, calculator.powerTwoNumbers(10, -1000), DELTA);
+        assertEquals(0, calculator.powerTwoNumbers(10, -1000), 0);
     }
 
-    @Test Sqrt_PositiveInput_Test
+    @Test
+    public void Sqrt_PositiveInput_Test() {
+        assertEquals(1.41421356237, calculator.sqrt(2), DELTA);
+    }
 }
